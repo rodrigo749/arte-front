bfd3f2373d037612c647f7325df3f65e8679b5aa
Rodrigo Neves Ottoboni <rodrigonottoni@outlook.com>
2024-06-03 09:27:01 -0300

    fix: fixed backend endpoin

--- src/app/services/tutorial.service.ts
+++ src/app/services/tutorial.service.ts
@@ -3,7 +3,8 @@ import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { Tutorial } from '../models/tutorial.model';
 
-const baseUrl = 'http://localhost:8080/api/tutorials';
+//const baseUrl = 'http://localhost:8080/api/tutorials'
+const baseUrl = 'https://api-render-teste.onrender.com/api/tutorials';
 
 @Injectable({
   providedIn: 'root',
